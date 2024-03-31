7a30da6a48774346b7c1f7c966f8bcd273c13a87
stephaniemukami <stephaniekariuki19@gmail.com>
2024-04-01 00:23:40 +0300

    initial commit

---
+++ src/app/order/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function page({}: Props) {
+  return (
+    <div> order page</div>
+  )
+}
+
+export default page
---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function page({}: Props) {
+  return (
+    <div> register page</div>
+  )
+}
+
+export default page
